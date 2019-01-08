856d800626251d579f4019dd4fbf3f53fa70ed37
Pav Sidhu <pav@pavsidhu.com>
2019-01-08 21:11:05 +0000

    Add .env support

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
+# Set environment variables from .env
+load_dotenv()
+
+# Connect to database
 database = create_engine("postgres://localhost:5432/postgres")
 Session = sessionmaker(bind=database)
 
