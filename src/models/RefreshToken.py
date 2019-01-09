9e3416c768600dd279e5bb23eea7a4766567f4de
Pav Sidhu <pav@pavsidhu.com>
2019-01-09 13:45:57 +0000

    Add refresh token model

---
+++ src/models/RefreshToken.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from models import BaseModel
+
+
+class RefreshToken(BaseModel):
+    __tablename__ = "refresh_token"
+
+    token_hash = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    client_id = Column(Integer, ForeignKey("clients.id"), nullable=False)
+
+    def __init__(self, token_hash, user_id, client_id):
+        self.token_hash = token_hash
+        self.user_id = user_id
+        self.client_id = client_id
+
+    def __repr__(self):
+        return "<RefreshToken(id='{}', token_hash='{}', user_id='{}', client_id='{}', created_at='{}', updated_at='{}')>".format(
+            self.id,
+            self.token_hash,
+            self.user_id,
+            self.client_id,
+            self.created_at,
+            self.updated_at,
+        )
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,4 +3,5 @@ from models.User import User
 from models.Quote import Quote
 
 from models.Emotion import Emotion
+from models.RefreshToken import RefreshToken
 from models.Client import Client
