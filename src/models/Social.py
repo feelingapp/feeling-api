bf73288bbbf9c56238476a072b9f97f763fe11cc
Pav Sidhu <pav@pavsidhu.com>
2019-01-09 13:52:59 +0000

    Add Social modal

---
+++ src/models/Social.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, ForeignKey, Integer
+
+from models import BaseModel
+
+
+class Social(BaseModel):
+    __tablename__ = "socials"
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    provider_id = Column(Integer, ForeignKey("social_providers.id"), nullable=False)
+    social_id = Column(Integer, nullable=False)
+
+    def __init__(self, user_id, provider_id, social_id):
+        self.user_id = user_id
+        self.provider_id = provider_id
+        self.social_id = social_id
+
+    def __repr__(self):
+        return "<Social(id='{}', user_id='{}' provider_id='{}', social_id='{}', created_at='{}', updated_at='{}')>".format(
+            self.id,
+            self.user_id,
+            self.provider_id,
+            self.social_id,
+            self.created_at,
+            self.updated_at,
+        )
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,7 @@
 from models.Base import Base, BaseModel
 from models.User import User
 from models.Quote import Quote
-
+from models.Social import Social
 from models.SocialProvider import SocialProvider
 from models.Emotion import Emotion
 from models.RefreshToken import RefreshToken
