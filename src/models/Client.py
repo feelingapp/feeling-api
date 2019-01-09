658f7144d94aceae05fda0c100a7ae536cef6f0f
Pav Sidhu <pav@pavsidhu.com>
2019-01-09 13:35:04 +0000

    Add Client model

---
+++ src/models/Client.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String
+
+from models import BaseModel
+
+
+class Client(BaseModel):
+    __tablename__ = "clients"
+
+    name = Column(String, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Clients(id='{}', name='{}', created_at='{}', updated_at='{}')>".format(
+            self.id, self.name, self.created_at, self.updated_at
+        )
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@ from models.Base import Base, BaseModel
 from models.User import User
 from models.Quote import Quote
 
+from models.Client import Client
