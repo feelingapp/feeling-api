760e45f822a5d792a938e181e4798d3567de3357
Pav Sidhu <pav@pavsidhu.com>
2019-01-14 14:33:40 +0000

    Add database decorator

---
+++ src/utils/decorators.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def database(function):
+    """Decorator to help connect to a database"""
+
+    def wrap_function(*args, **kwargs):
+        # Set environment variables from .env
+        load_dotenv()
+
+        # Connect to database
+        database_url = os.getenv("DATABASE_URL")
+        engine = create_engine(database_url)
+
+        Session = sessionmaker(bind=engine)
+
+        # create a Session
+        session = Session()
+
+        # Call function
+        response = function(session, *args, **kwargs)
+
+        # Close database connection
+        engine.dispose()
+
+        return response
+
+    return wrap_function
