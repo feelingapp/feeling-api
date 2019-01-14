aa03fccab1f539e31492b333329a49c995f7fa65
Pav Sidhu <pav@pavsidhu.com>
2019-01-14 18:37:35 +0000

    Add JSON schema validate decorator

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -1,6 +1,9 @@
 import os
+import re
+import json
 
 from dotenv import load_dotenv
+from jsonschema import Draft4Validator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -30,3 +33,39 @@ def database(function):
         return response
 
     return wrap_function
+
+
+def validate(schema):
+    """Decorator to help validate the body of the event"""
+
+    def parse_error_message(message):
+        """Removes escaped characters from error message"""
+
+        return re.sub(r"(\\n|\\t|\\')", "", message)
+
+    def decorator(function):
+        def wrap_function(*args):
+            body = json.loads(args[0]["body"])
+
+            # Create JSON schema validator
+            validator = Draft4Validator(schema)
+
+            # Generate errors
+            errors = [
+                {
+                    "type": "validation-error",
+                    "message": parse_error_message(error.message),
+                }
+                for error in validator.iter_errors(body)
+            ]
+
+            # Return error to client if body is invalid
+            if errors:
+                return {"statusCode": 400, "body": {"errors": errors}}
+
+            # Call function
+            return function(*args, errors)
+
+        return wrap_function
+
+    return decorator
