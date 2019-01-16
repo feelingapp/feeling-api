1375fe9eada5e7f0dd9c21d8976004147fb01671
Pav Sidhu <pav@pavsidhu.com>
2019-01-16 12:56:33 +0000

    Add emotions to table in setup

--- src/setup.py
+++ src/setup.py
@@ -2,8 +2,10 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from src.models import Base
+from src.models import Base, Emotion as EmotionTable
+from src.consts import Emotion
 
 # Set environment variables from .env
 load_dotenv()
@@ -15,5 +17,19 @@ database = create_engine(database_url)
 # Create tables if they do not exist
 Base.metadata.create_all(database)
 
-# Close connection
+# create a Session
+Session = sessionmaker(bind=database)
+session = Session()
+
+
+# Add emotions to Emotion table
+for emotion in Emotion:
+    session.add(EmotionTable(emotion.name))
+
+# Finish and close session
+session.commit()
+session.close()
+
+# End database connection
 database.dispose()
+
