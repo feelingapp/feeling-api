318226cd6752473860ae1ad56a67373e25964634
Pav Sidhu <pav@pavsidhu.com>
2019-01-17 19:11:52 +0000

    Add create feeling method

--- src/functions/feeling.py
+++ src/functions/feeling.py
@@ -1,5 +1,18 @@
-from src.models import Feeling
-from src.utils.decorators import database, token_required
+import json
+
+from src.consts import Emotion
+from src.models import Feeling, Hashtag
+from src.utils.decorators import database, token_required, validate
+
+schema = {
+    "type": "object",
+    "properties": {
+        "emotion": {"type": "string", "enum": Emotion.list()},
+        "description": {"type": "string"},
+        "hashtags": {"type": "array", "items": {"type": "string"}, "uniqueItems": True},
+    },
+    "required": ["emotion"],
+}
 
 
 @database
@@ -16,6 +29,30 @@ def get(event, context, session):
         return {"statusCode": 404}
 
     return {"statusCode": 200, "body": feeling.toJson()}
+
+
+@database
+@token_required
+@validate(schema)
+def post(event, context, session):
+    body = json.loads(event["body"])
+    user_id = event["user_id"]
+
+    # Create a new feeling in the database
+    emotion = Emotion[body["emotion"]]
+    description = body["description"]
+    feeling = Feeling(emotion, description, user_id)
+
+    # If hashtags, add them to the database
+    if body.get("hashtags"):
+        for name in body["hashtags"]:
+            hashtag = Hashtag(name)
+            feeling.hashtags.append(hashtag)
+
+    session.add(feeling)
+    session.commit()
+
+    return {"statusCode": 200}
 
 
 @database
