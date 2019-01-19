b8b9c18a27da5accfdf8c7bdbadfb3f7aa7167e1
Pav Sidhu <pav@pavsidhu.com>
2019-01-19 18:59:07 +0000

    Change feeling model to store hashtags in an array

--- src/models/Feeling.py
+++ src/models/Feeling.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, ARRAY
 from sqlalchemy.orm import relationship
 
 from src.consts import Emotion
@@ -11,12 +11,13 @@ class Feeling(BaseModel):
 
     emotion_id = Column(Integer, ForeignKey("emotions.id"), nullable=False)
     description = Column(String)
+    hashtags = Column(ARRAY(String))
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
-    hashtags = relationship("Hashtag", secondary="feeling_hashtags", backref="feelings")
 
-    def __init__(self, emotion: Emotion, description, user_id):
+    def __init__(self, emotion: Emotion, description, hashtags, user_id):
         self.emotion_id = emotion.value
         self.description = description
+        self.hashtags = hashtags
         self.user_id = user_id
 
     @property
@@ -28,16 +29,18 @@ class Feeling(BaseModel):
             "id": str(self.id),
             "emotion": Emotion(self.emotion_id).name,
             "description": self.description,
-            "user_id": self.user_id,
+            "hashtags": self.hashtags,
+            "user_id": str(self.user_id),
             "created_at": str(self.created_at),
             "updated_at": str(self.updated_at),
         }
 
     def __repr__(self):
-        return "<Feeling(id='{}', emotion_id='{}', description='{}', user_id='{}', created_at='{}', updated_at='{}')>".format(
+        return "<Feeling(id='{}', emotion_id='{}', description='{}', hashtags='{}', user_id='{}', created_at='{}', updated_at='{}')>".format(
             self.id,
             self.emotion_id,
             self.description,
+            self.hashtags,
             self.user_id,
             self.created_at,
             self.updated_at,
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,8 +1,6 @@
 from src.models.Base import Base, BaseModel
 from src.models.User import User
 from src.models.Feeling import Feeling
-from src.models.Hashtag import Hashtag
-from src.models.FeelingHashtag import FeelingHashtag
 from src.models.Quote import Quote
 from src.models.Social import Social
 from src.models.SocialProvider import SocialProvider
