2000130641335fa9c65288d481880df98d7c53fa
Pav Sidhu <pav@pavsidhu.com>
2019-07-25 22:25:36 +0100

    Add access token model

---
+++ src/models/AccessToken.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+import jwt
+
+
+class AccessToken:
+    # Equivalent to 2 hours
+    TOKEN_LIFE = 7200
+
+    token = None
+
+    def __init__(self, user_id):
+        expires_in = int(time.time()) + self.TOKEN_LIFE
+
+        payload = {"sub": user_id, "exp": expires_in}
+
+        self.token = jwt.encode(payload, os.getenv("SECRET_KEY"), algorithm="HS256")
+
+    @staticmethod
+    def has_expired(self, token):
+        payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithm="HS256")
+
+        return payload["expires_in"] + self.TOKEN_LIFE < time.time()
